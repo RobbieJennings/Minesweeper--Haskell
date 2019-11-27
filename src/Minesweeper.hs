f3fa4bc59561dc99f0bb28bda12afa7838ea450d
Robbie <robbie.jennings97@gmail.com>
2019-11-27 21:45:32 +0000

    Tidy up

--- src/Minesweeper.hs
+++ src/Minesweeper.hs
@@ -6,14 +6,16 @@ import System.Random
 
 data Cell = Cell { uncovered    :: Bool
                  , flagged      :: Bool
-                 , bomb         :: Bool
+                 , mine         :: Bool
                  , number       :: Int
                  , neighbours   :: [Int]
                  }
 
+-- Converts x and y coordinates to index in grid
 get_index :: Int -> Int -> Int -> Int
 get_index x y size = x+(y*size)
 
+-- Gets list of indices of neighbouring cells in a grid using x and y coordinates
 get_neighbours :: Int -> Int -> Int -> [Int]
 get_neighbours x y size
   | size < 2                    = []
@@ -27,43 +29,50 @@ get_neighbours x y size
   | y == size-1                 = [(get_index (x-1) (y-1) size), (get_index (x-1) y size), (get_index x (y-1) size), (get_index (x+1) (y-1) size), (get_index (x+1) y size)]
   | otherwise                   = [(get_index (x-1) (y-1) size), (get_index (x-1) y size), (get_index (x-1) (y+1) size), (get_index x (y-1) size), (get_index x (y+1) size), (get_index (x+1) (y-1) size), (get_index (x+1) y size), (get_index (x+1) (y+1) size)]
 
+-- Gets number of neighbouring mines of a cell in a grid using x and y coordinates
 get_number :: Int -> Int -> Int -> [Int] -> Int
-get_number x y size bombs = do
+get_number x y size mines = do
   let neighbours = get_neighbours x y size
-  let adjacent_bombs = neighbours `intersect` bombs
-  length adjacent_bombs
+  let adjacent_mines = neighbours `intersect` mines
+  length adjacent_mines
 
-make_cell :: Int -> Int -> Int -> [Int] -> Cell
-make_cell x y size bombs = do
-  let bomb = is_bomb x y size bombs
-  let neighbours = get_neighbours x y size
-  let number = get_number x y size bombs
-  Cell {uncovered=False, flagged=False, bomb=bomb, number=number, neighbours=neighbours}
-
+-- Generates a random list of unique indices for mines and produces new generator
 make_randoms :: Int -> Int -> [Int] -> StdGen -> ([Int], StdGen)
 make_randoms range count nums generator
   | (length nums) >= count = do
-      let (_, new_gen) = randomR (0, (range-1)) generator
-      (nums, new_gen)
+      let (_, new_generator) = randomR (0, (range-1)) generator
+      (nums, new_generator)
   | otherwise = do
       let all_nums = [0..range]
       let available_nums = all_nums \\ nums
-      let (index, new_gen) = randomR (0, (length available_nums) - 1) generator
+      let (index, new_generator) = randomR (0, (length available_nums) - 1) generator
       let num = available_nums!!index
-      make_randoms range count (nums++[num]) new_gen
+      make_randoms range count (nums++[num]) new_generator
 
-make_bombs :: Int -> Int -> StdGen -> ([Int], StdGen)
-make_bombs size count generator = make_randoms (size^2) count [] generator
+-- Makes a list of random mines and a new generator
+make_mines :: Int -> Int -> StdGen -> ([Int], StdGen)
+make_mines size count generator = make_randoms (size^2) count [] generator
 
-is_bomb :: Int -> Int -> Int -> [Int] -> Bool
-is_bomb x y size bombs = (get_index x y size) `elem` bombs
+-- Checks if x and y coordinates correspond to a mine
+is_mine :: Int -> Int -> Int -> [Int] -> Bool
+is_mine x y size mines = (get_index x y size) `elem` mines
 
+-- Makes a cell using x and y coordinates and mine array
+make_cell :: Int -> Int -> Int -> [Int] -> Cell
+make_cell x y size mines = do
+  let mine = is_mine x y size mines
+  let neighbours = get_neighbours x y size
+  let number = get_number x y size mines
+  Cell {uncovered=False, flagged=False, mine=mine, number=number, neighbours=neighbours}
+
+-- Makes a grid of cells
 make_grid :: Int -> [Int] -> [Cell]
-make_grid size bombs = do
+make_grid size mines = do
   let coordinates = [(y, x) | x <- [0..size-1], y <- [0..size-1]]
-  let cell (x, y) = make_cell x y size bombs
+  let cell (x, y) = make_cell x y size mines
   map cell coordinates
 
+-- Finds all neighbours of a cell in a grid that have yet to be uncovered
 hidden_neighbours :: [Cell] -> Int -> [Int]
 hidden_neighbours grid index = do
   let cell = grid!!index
@@ -71,18 +80,20 @@ hidden_neighbours grid index = do
   let hidden a = not $ uncovered $ grid!!a
   filter (hidden) all_neighbours
 
+-- Uncovers individual cell
 uncover_cell :: Cell -> Cell
 uncover_cell a = Cell { uncovered = True
                       , flagged = False
-                      , bomb = bomb a
+                      , mine = mine a
                       , number = number a
                       , neighbours = neighbours a
                       }
 
+-- Uncovers cell in grid and propogates if number is 0
 uncover :: [Cell] -> Int -> [Cell]
 uncover grid index = do
   let cell = uncover_cell $ grid!!index
-  if ((number cell == 0) && (not $ bomb cell)) then
+  if ((number cell == 0) && (not $ mine cell)) then
     do
       let first = take index grid
       let second = drop (index+1) grid
@@ -95,21 +106,23 @@ uncover grid index = do
       let second = drop (index+1) grid
       first ++ ([cell] ++ second)
 
+-- Flags individual cell
 flag_cell :: Cell -> Cell
 flag_cell a | uncovered a = a
             | flagged a   = Cell { uncovered = False
                                  , flagged = False
-                                 , bomb = bomb a
+                                 , mine = mine a
                                  , neighbours = neighbours a
                                  , number = number a
                                  }
             | otherwise   = Cell { uncovered = False
                                  , flagged = True
-                                 , bomb = bomb a
+                                 , mine = mine a
                                  , neighbours = neighbours a
                                  , number = number a
                                  }
 
+-- Flags cell in grid
 flag :: [Cell] -> Int -> [Cell]
 flag grid index = do
   let cell = flag_cell $ grid!!index
@@ -117,34 +130,41 @@ flag grid index = do
   let second = drop (index+1) grid
   first ++ ([cell] ++ second)
 
+-- Checks if individual cell satisfies winning condition
 win_cell :: Cell -> Bool
-win_cell a = ((uncovered a) && (not (bomb a))) || ((not (uncovered a)) && (bomb a))
+win_cell a = ((uncovered a) && (not (mine a))) || ((not (uncovered a)) && (mine a))
 
+-- Checks if all cells in grid satisfy winning condition
 win :: [Cell] -> Int -> Bool
 win grid size = do
   let winners = map win_cell grid
   let count = length $ filter (==True) winners
   count == (size^2)
 
+-- Checks if indivual cell satisfies losing condition
 lose_cell :: Cell -> Bool
-lose_cell a = (uncovered a) && (bomb a)
+lose_cell a = (uncovered a) && (mine a)
 
+-- Checks if all cells in grid satisfy losing condition
 lose :: [Cell] -> Bool
 lose grid = do
   let losers = map lose_cell grid
   True `elem` losers
 
+-- Resets indivual cell to uncovered state
 reset_cell :: Cell -> Cell
 reset_cell a = Cell { uncovered = False
                      , flagged = False
-                     , bomb = bomb a
+                     , mine = mine a
                      , neighbours = neighbours a
                      , number = number a
                      }
 
+-- Resets all cells in grid to uncovered state
 reset :: [Cell] -> [Cell]
 reset grid = map reset_cell grid
 
+-- Makes safest possible move in a grid of cells
 ai :: [Cell] -> [Cell]
 ai grid = do
   uncover grid 0
--- src/UI.hs
+++ src/UI.hs
@@ -9,19 +9,23 @@ import qualified Graphics.UI.Threepenny as UI
 import Graphics.UI.Threepenny.Core as Core
 import Graphics.Gloss.Interface.Environment
 
+-- Converts number to pixel string for threepenny
 pixels :: Int -> String
 pixels size = (show size) ++ "px"
 
+-- Shows state of individual cell
 show_cell :: Cell -> String
 show_cell a
   | flagged a             = "F"
-  | uncovered a && bomb a = "X"
+  | uncovered a && mine a = "X"
   | uncovered a           = show (number a)
   | otherwise             = "?"
 
+-- Shows state of all cells in grid
 show_grid :: [Cell] -> [String]
 show_grid a = map show_cell a
 
+-- Plays game inside of terminal
 terminal :: [Cell] -> Int -> IO ()
 terminal grid size
   | win grid size = putStrLn "You Win"
@@ -74,14 +78,16 @@ terminal grid size
       do
         terminal grid size
 
+-- Starts a game inside the terminal
 terminal_play :: Int -> Int -> IO ()
 terminal_play size num_mines = do
   generator <- newStdGen
-  let (bombs, _) = make_bombs size num_mines generator
-  let grid = make_grid size bombs
+  let (mines, _) = make_mines size num_mines generator
+  let grid = make_grid size mines
   mapM_ print $ chunksOf size $ show_grid grid
   terminal grid size
 
+-- Makes a button corresponding to a cell in a grid
 cell_button :: [Cell] -> Int -> Int -> Int -> Window -> UI Element
 cell_button grid size index button_size window
   | (not $ uncovered (grid!!index)) && (not $ flagged (grid!!index)) = do
@@ -102,7 +108,7 @@ cell_button grid size index button_size window
                                        , ("height", (pixels button_size))
                                        ]
       return button
-  | bomb (grid!!index) = do
+  | mine (grid!!index) = do
       button <- UI.button # set text "X"
                           # set (attr "class") ("button")
                           # set (attr "oncontextmenu") ("return false;")
@@ -130,6 +136,7 @@ cell_button grid size index button_size window
                                        ]
       return button
 
+-- Makes a clickable button corresponding to a cell in a grid
 clickable_cell :: [Cell] -> Int -> Int -> Int -> Int -> StdGen -> Window -> UI Element
 clickable_cell grid size num_mines index button_size generator window = do
   button <- cell_button grid size index button_size window
@@ -145,6 +152,7 @@ clickable_cell grid size num_mines index button_size generator window = do
     gui new_grid size num_mines button_size generator window
   return button
 
+-- Makes a grid of clickable buttons corresponding to a grid of cells
 clickable_grid :: [Cell] -> Int -> Int -> Int -> StdGen -> Window -> UI Element
 clickable_grid grid size num_mines button_size generator window = do
   let button a = clickable_cell grid size num_mines a button_size generator window
@@ -152,11 +160,13 @@ clickable_grid grid size num_mines button_size generator window = do
   button_grid <- UI.grid $ chunksOf size buttons
   return button_grid
 
+-- Makes an unclickable button corresponding to a cell in a grid
 unclickable_cell :: [Cell] -> Int -> Int -> Int -> Window -> UI Element
 unclickable_cell grid size index button_size window = do
   button <- cell_button grid size index button_size window
   return button
 
+-- Makes a grid of unclickable buttons corresponding to a grid of cells
 unclickable_grid :: [Cell] -> Int -> Int -> Window -> UI Element
 unclickable_grid grid size button_size window = do
   let button a = unclickable_cell grid size a button_size window
@@ -164,6 +174,7 @@ unclickable_grid grid size button_size window = do
   button_grid <- UI.grid $ chunksOf size buttons
   return button_grid
 
+-- Makes a button which plays an AI move
 ai_button :: [Cell] -> Int -> Int -> Int -> StdGen -> Window -> UI Element
 ai_button grid size num_mines button_size generator window = do
   button <- UI.button #+ [string "Make Move"]
@@ -176,6 +187,7 @@ ai_button grid size num_mines button_size generator window = do
     gui new_grid size num_mines button_size generator window
   return button
 
+-- Makes a winner button which starts new game
 winner_button :: Int -> Int -> Int -> StdGen -> Window -> UI Element
 winner_button size num_mines button_size generator window = do
   button <- UI.button #+ [string ":)"]
@@ -187,6 +199,7 @@ winner_button size num_mines button_size generator window = do
     start_gui size num_mines button_size generator window
   return button
 
+-- Makes a loser button which starts new game
 loser_button :: [Cell] -> Int -> Int -> Int -> StdGen -> Window -> UI Element
 loser_button grid size num_mines button_size generator window = do
   button <- UI.button #+ [string ":("]
@@ -198,6 +211,7 @@ loser_button grid size num_mines button_size generator window = do
     start_gui size num_mines button_size generator window
   return button
 
+-- Plays a game using threepenny and verifies all buttons are on screen (This does not always happen due to threepenny issues)
 gui :: [Cell] -> Int -> Int -> Int -> StdGen -> Window -> UI ()
 gui grid size num_mines button_size generator window
   | win grid size = do
@@ -225,19 +239,19 @@ gui grid size num_mines button_size generator window
         gui grid size num_mines button_size generator window
       else return ()
 
+-- Starts a new game in threepenny
 start_gui :: Int -> Int -> Int -> StdGen -> Window -> UI ()
 start_gui size num_mines button_size generator = do
-  let (bombs, new_gen) = make_bombs size num_mines generator
-  let grid = make_grid size bombs
+  let (mines, new_gen) = make_mines size num_mines generator
+  let grid = make_grid size mines
   gui grid size num_mines button_size new_gen
 
-
+-- Starts first game in threepenny
 gui_play :: Int -> Int -> IO ()
 gui_play size num_mines = do
   (width, height) <- getScreenSize
   let screen_size = min width height
   let button_ratio = (screen_size `div` (size + 1))
   let button_size = floor (fromIntegral (button_ratio) * 0.8)
-
   generator <- getStdGen
   startGUI defaultConfig $ start_gui size num_mines button_size generator
